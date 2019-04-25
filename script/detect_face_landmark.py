0d0edb2e9ed2ac219df83ec84a8c3134eaa034e9
carl_liu <yirenliu0909@gmail.com>
2019-04-25 15:18:47 +0800

    add cpp read bag file

--- script/detect_face_landmark.py
+++ script/detect_face_landmark.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import rospy
-from sensor_msgs.msg import Image, PointCloud2
+from sensor_msgs.msg import Image, PointCloud2, PointField
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import face_alignment
@@ -8,20 +8,52 @@ import numpy as np
 
 
 def node():
+    rospy.init_node('detect_face_landmark', anonymous=True)
     cap = cv2.VideoCapture(0)
     fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, device='cuda:0', flip_input=True)
+    pub_image = rospy.Publisher("image_raw", Image, queue_size=10)
+    pub_landmark = rospy.Publisher("landmark", PointCloud2, queue_size=10)
+    bridge = CvBridge()
+    seq = 0
 
-    while(True):
+    while not rospy.is_shutdown():
         ret, frame = cap.read()
         preds = fa.get_landmarks(frame)
 
-        show_result = np.copy(frame)
-
         if preds and len(preds) == 1:
+            show_result = np.copy(frame)
+            timestamp = rospy.Time().now()
+
+            # pub landmark
+            lm_msg = PointCloud2()
+            lm_msg.header.stamp = timestamp
+            lm_msg.header.seq = seq
+            lm_msg.height = 1
+            lm_msg.width = len(preds[0])
+            lm_msg.fields = [PointField('x', 0, PointField.FLOAT32, 1),
+                             PointField('y', 4, PointField.FLOAT32, 1)]
+            lm_msg.is_bigendian = False
+            lm_msg.is_dense = True
+            lm_msg.point_step = 8
+            lm_msg.row_step = 8 * preds[0].shape[0]
+            lm_msg.data = np.asarray(preds[0], np.float32).tostring()
+
+            pub_landmark.publish(lm_msg)
+
+            # pub raw image
+            try:
+                img_msg = bridge.cv2_to_imgmsg(frame, 'bgr8')
+                img_msg.header.stamp = timestamp
+                img_msg.header.seq = seq
+                pub_image.publish(img_msg)
+            except CvBridgeError as e:
+                print(e)
+
             for lm in preds[0]:
                 cv2.circle(show_result, (lm[0], lm[1]), 2, (0, 255, 0), -1)
-
             cv2.imshow('result', show_result)
+
+            ++seq
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
---
+++ src/face_landmark_sfm_node.cpp
@@ -0,0 +1,82 @@
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <cv_bridge/cv_bridge.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+
+#include <boost/foreach.hpp>
+
+class DataMgr {
+public:
+    void Callback(const sensor_msgs::ImageConstPtr& img_msg,
+                  const sensor_msgs::PointCloud2ConstPtr& lm_msg)
+    {
+        cv::Mat img = cv_bridge::toCvCopy(img_msg, "bgr8")->image;
+        std::vector<cv::Point2f> landmarks(lm_msg->width);
+
+        for(int i = 0, n = landmarks.size(); i < n; ++i) {
+            std::memcpy(&landmarks[i].x, lm_msg->data.data() + 8 * i, sizeof(float));
+            std::memcpy(&landmarks[i].y, lm_msg->data.data() + 8 * i + 4, sizeof(float));
+        }
+
+        v_img.emplace_back(img);
+        v_landmarks.emplace_back(std::move(landmarks));
+    };
+
+    void Process() {
+        // FaceSfm.Process(K, D, v_img, v_landmarks)
+    }
+
+    std::vector<cv::Mat> v_img;
+    std::vector<std::vector<cv::Point2f>> v_landmarks;
+    cv::Mat K, D;
+    // FaceSfm
+};
+
+template <class T>
+class BagSubscriber : public message_filters::SimpleFilter<T>
+{
+public:
+    void NewMessage(const boost::shared_ptr<T const>& msg) {
+        this->signalMessage(msg);
+    }
+};
+
+int main(int argc, char** argv) {
+    ros::Time::init();
+    DataMgr data_mgr;
+    rosbag::Bag bag;
+    bag.open(argv[1], rosbag::bagmode::Read);
+
+    std::vector<std::string> topics;
+    topics.emplace_back("/image_raw");
+    topics.emplace_back("/landmark");
+
+    rosbag::View view(bag, rosbag::TopicQuery(topics));
+
+    BagSubscriber<sensor_msgs::Image> sub_img;
+    BagSubscriber<sensor_msgs::PointCloud2> sub_lm;
+    message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::PointCloud2> sync(sub_img, sub_lm, 10);
+
+    sync.registerCallback(boost::bind(&DataMgr::Callback, &data_mgr, _1, _2));
+
+    BOOST_FOREACH(rosbag::MessageInstance const m, view) {
+        sensor_msgs::ImageConstPtr img_msg = m.instantiate<sensor_msgs::Image>();
+        if(img_msg) {
+            sub_img.NewMessage(img_msg);
+        }
+
+        sensor_msgs::PointCloud2ConstPtr lm_msg = m.instantiate<sensor_msgs::PointCloud2>();
+        if(lm_msg) {
+            sub_lm.NewMessage(lm_msg);
+        }
+    }
+
+    bag.close();
+    data_mgr.Process();
+    return 0;
+}
