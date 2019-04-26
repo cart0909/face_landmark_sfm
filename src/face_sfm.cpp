d5f9a0b23f7d3a7aa081796498f9c660e93a4c80
carl_liu <yirenliu0909@gmail.com>
2019-04-26 18:15:19 +0800

    system connect success

--- src/face_sfm.cpp
+++ src/face_sfm.cpp
@@ -1,5 +1,6 @@
 #include "face_sfm.h"
 #include <ceres/ceres.h>
+#include <opencv2/core/eigen.hpp>
 #include "local_parameterization.h"
 #include "factor.h"
 
@@ -36,6 +37,9 @@ void FaceSfm::Process(const std::vector<cv::Mat>& v_img, const std::vector<std::
 
         frames.emplace_back(frame);
     }
+
+    InitGraph();
+    GlobalBA();
 }
 
 void FaceSfm::InitGraph() {
@@ -55,9 +59,90 @@ void FaceSfm::InitGraph() {
             best_frame_idx = i;
         }
     }
+
+    // essential matrix and recover pose
+    {
+        std::vector<cv::Point2d> point1, point2;
+        cv::Mat K = cv::Mat::eye(3, 3, CV_64F) * camera->f();
+        for(int i = 0; i < landmarks.size(); ++i) {
+            point1.emplace_back(landmarks[i].pt_n_per_frame[0](0) * camera->f(),
+                                landmarks[i].pt_n_per_frame[0](1) * camera->f());
+            point2.emplace_back(landmarks[i].pt_n_per_frame[best_frame_idx](0) * camera->f(),
+                                landmarks[i].pt_n_per_frame[best_frame_idx](1) * camera->f());
+        }
+        cv::Mat mask;
+        cv::Mat E = cv::findEssentialMat(point1, point2, K, cv::RANSAC, 0.999, 1.0, mask);
+        cv::Mat R, t; // R10, t10
+        cv::recoverPose(E, point1, point2, K, R, t, mask);
+        Eigen::Matrix3d R10;
+        Eigen::Vector3d t10;
+        cv::cv2eigen(R, R10);
+        cv::cv2eigen(t, t10);
+        Sophus::SE3d T10(R10, t10);
+        frames[best_frame_idx].Twc = frames[0].Twc * T10.inverse();
+    }
+
+    // triangulation
+    {
+        int iter_idx[2] = {0, best_frame_idx};
+        Sophus::SE3d Tcw[2] = {frames[0].Twc.inverse(), frames[best_frame_idx].Twc.inverse()};
+        Eigen::Matrix<double, 3, 4> P[2];
+        P[0] << Tcw[0].so3().matrix(), Tcw[0].translation();
+        P[1] << Tcw[1].so3().matrix(), Tcw[1].translation();
+
+        for(auto& lm : landmarks) {
+            Eigen::Matrix4d A;
+            for(int i = 0; i < 2; ++i) {
+                int frame_idx = iter_idx[i];
+                Eigen::Vector3d& pt_j = lm.pt_n_per_frame[frame_idx];
+                A.col(2 * i)     = pt_j(0) * P[i].row(2) - P[i].row(0);
+                A.col(2 * i + 1) = pt_j(1) * P[i].row(2) - P[i].row(1);
+            }
+
+            Eigen::JacobiSVD<Eigen::MatrixXd> svd(A, Eigen::ComputeThinV);
+            Eigen::Vector4d x = svd.matrixV().rightCols(1);
+            Eigen::Vector3d x3Dw = x.head<3>() / x(3);
+            lm.x3Dw = x3Dw;
+        }
+    }
+
+    // PnP calc other frame poses
+    {
+        std::vector<cv::Point3d> object_points;
+        for(auto& lm : landmarks) {
+            object_points.emplace_back(lm.x3Dw(0), lm.x3Dw(1), lm.x3Dw(2));
+        }
+
+        for(int i = 1; i < frames.size(); ++i) {
+            if(i == best_frame_idx)
+                continue;
+
+            std::vector<cv::Point2d> image_points;
+            cv::Mat K = cv::Mat::eye(3, 3, CV_64F), rvec, tvec;
+
+            for(auto& lm : landmarks) {
+                image_points.emplace_back(lm.pt_n_per_frame[i](0), lm.pt_n_per_frame[i](1));
+            }
+
+            cv::solvePnPRansac(object_points, image_points, K, cv::noArray(), rvec, tvec, false,
+                               100, 8.0 / camera->f());
+
+            cv::Mat R;
+            cv::Rodrigues(rvec, R);
+
+            Eigen::Matrix3d Rcw;
+            Eigen::Vector3d tcw;
+            cv::cv2eigen(R, Rcw);
+            cv::cv2eigen(tvec, tcw);
+            Sophus::SE3d Tcw(Rcw, tcw);
+
+            frames[i].Twc = Tcw.inverse();
+        }
+    }
 }
 
 void FaceSfm::GlobalBA() {
+    LOG(WARNING) << "GlobalBA";
     ceres::Problem problem;
     ceres::LossFunction* loss_function = new ceres::CauchyLoss(1);
     ceres::LocalParameterization* local_para_se3 = new LocalParameterizationSE3();
@@ -87,6 +172,7 @@ void FaceSfm::GlobalBA() {
     options.trust_region_strategy_type = ceres::LEVENBERG_MARQUARDT;
     options.num_threads = 1;
     ceres::Solver::Summary summary;
+    LOG(WARNING) << "Solve...";
     ceres::Solve(options, &problem, &summary);
 
     LOG(INFO) << summary.FullReport() << std::endl;
